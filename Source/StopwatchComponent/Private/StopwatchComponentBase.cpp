369a9eeb978607212f3e36b7ce9de8787ceb2da4
Snow <johanyang0411@gmail.com>
2024-02-25 22:56:55 +0800

    Add Timeline Functionality And Create BP Class

--- Source/StopwatchComponent/Private/StopwatchComponentBase.cpp
+++ Source/StopwatchComponent/Private/StopwatchComponentBase.cpp
@@ -18,9 +18,26 @@ UStopwatchComponentBase::UStopwatchComponentBase()
 void UStopwatchComponentBase::BeginPlay()
 {
 	Super::BeginPlay();
+	FOnTimelineEvent EventTrackOnFinished;
+	EventTrackOnFinished.BindUFunction(this, FName("OnTimelineFinished"));
+	Timeline.AddEvent(1.0, EventTrackOnFinished);
 
-	// ...
-	
+	FOnTimelineEvent EventTrackOnStart;
+	EventTrackOnStart.BindUFunction(this, FName("OnTimelineStarted"));
+	Timeline.AddEvent(0.0, EventTrackOnStart);
+
+	Timeline.SetPropertySetObject(this);
+	Timeline.SetDirectionPropertyName(FName("TimelineDirection"));
+
+	if (CurveFloat) {
+		FOnTimelineFloat TimelineProgress;
+		TimelineProgress.BindUFunction(this, FName("OutputCurveFloatValue"));
+		Timeline.AddInterpFloat(CurveFloat, TimelineProgress, "test", "TimelineProgress");
+	}
+	else {
+		if (GEngine)
+			GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, FString::Printf(TEXT("Error : CurveFloat Invalid")));
+	}
 }
 
 
@@ -28,7 +45,146 @@ void UStopwatchComponentBase::BeginPlay()
 void UStopwatchComponentBase::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
+	Timeline.TickTimeline(DeltaTime);
 }
 
+void UStopwatchComponentBase::PlayTimelineFromStart(float duration)
+{
+	if (duration <= 0) {
+		TimelineDirection = ETimelineDirection::Forward;
+		OnTimelineFinished();
+	}
+	else {
+		playrate = 1.0 / duration;
+		Timeline.SetPlayRate(playrate);
+		Timeline.PlayFromStart();
+	}
+}
+
+void UStopwatchComponentBase::PlayTimeline(float duration)
+{
+	if (duration <= 0) {
+		TimelineDirection = ETimelineDirection::Forward;
+		OnTimelineFinished();
+	}
+	else {
+		playrate = 1.0 / duration;
+		Timeline.SetPlayRate(playrate);
+		Timeline.Play();
+	}
+}
+
+void UStopwatchComponentBase::PlayTimelineFromPosition(float duration, float position)
+{
+	Timeline.SetPlaybackPosition(position, true, true);
+	PlayTimeline(duration);
+}
+
+void UStopwatchComponentBase::ReverseTimelineFromEnd(float duration)
+{
+	if (duration <= 0) {
+		TimelineDirection = ETimelineDirection::Backward;
+		OnTimelineStarted();
+	}
+	else {
+		playrate = 1.0 / duration;
+		Timeline.SetPlayRate(playrate);
+		Timeline.ReverseFromEnd();
+	}
+}
+
+void UStopwatchComponentBase::ReverseTimeline(float duration)
+{
+	if (duration <= 0) {
+		TimelineDirection = ETimelineDirection::Backward;
+		OnTimelineStarted();
+	}
+	else {
+		playrate = 1.0 / duration;
+		Timeline.SetPlayRate(playrate);
+		Timeline.Reverse();
+	}
+}
+
+void UStopwatchComponentBase::ReverseTimelineFromPosition(float duration, float position)
+{
+	Timeline.SetPlaybackPosition(position, true, true);
+	ReverseTimeline(duration);
+}
+
+void UStopwatchComponentBase::PlaySpecificPosition(float position)
+{
+	StopTimeline();
+	Timeline.SetPlaybackPosition(position, true, true);
+}
+
+void UStopwatchComponentBase::StopTimeline()
+{
+	if (Timeline.IsPlaying()) {
+		Timeline.Stop();
+	}
+}
+
+float UStopwatchComponentBase::GetPlayBackPosition()
+{
+	return Timeline.GetPlaybackPosition();
+}
+
+void UStopwatchComponentBase::SetFloatCurve(UCurveFloat* NewCurveFloat)
+{
+	if (NewCurveFloat != nullptr)
+		Timeline.SetFloatCurve(NewCurveFloat, FName("TimelineProgress"));
+}
+
+void UStopwatchComponentBase::SetTimelineLooping(bool NewLooping)
+{
+	Timeline.SetLooping(NewLooping);
+}
+
+bool UStopwatchComponentBase::GetTimelineLooping()
+{
+	return Timeline.IsLooping();
+}
+
+bool UStopwatchComponentBase::GetTimelinePlaying()
+{
+	return Timeline.IsPlaying();
+}
+
+bool UStopwatchComponentBase::GetTimelineReversing()
+{
+	return Timeline.IsReversing();
+}
+
+float UStopwatchComponentBase::GetPlayRate()
+{
+	return Timeline.GetPlayRate();
+}
+
+void UStopwatchComponentBase::SetPlayRate(float NewPlayRate)
+{
+	Timeline.SetPlayRate(NewPlayRate);
+}
+
+void UStopwatchComponentBase::OnTimelineStarted_Implementation()
+{
+	if (GEngine)
+		GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, FString::Printf(TEXT("Timeline Started")));
+}
+
+void UStopwatchComponentBase::OnTimelineFinished_Implementation()
+{
+	if (GEngine)
+		GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, FString::Printf(TEXT("Timeline Finished")));
+}
+
+void UStopwatchComponentBase::OutputCurveFloatValue_Implementation(float Value)
+{
+	FString TheFloatStr = FString::SanitizeFloat(Value);
+	if (GEngine)
+		GEngine->AddOnScreenDebugMessage(-1, 2.0f, FColor::Yellow, TheFloatStr);
+
+	//if (Timeline.GetTimelineDirectionEnum.Equal(ETimelineDirection::Forward)) {
+
+	//}
+}
--- Source/StopwatchComponent/Public/StopwatchComponentBase.h
+++ Source/StopwatchComponent/Public/StopwatchComponentBase.h
@@ -4,10 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
+#include "Components/TimelineComponent.h"
 #include "StopwatchComponentBase.generated.h"
 
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(Blueprintable)
 class STOPWATCHCOMPONENT_API UStopwatchComponentBase : public UActorComponent
 {
 	GENERATED_BODY()
@@ -16,13 +17,81 @@ public:
 	// Sets default values for this component's properties
 	UStopwatchComponentBase();
 
+	UFUNCTION(BlueprintCallable, Category = "TimelineActorComp")
+		void PlayTimelineFromStart(float duration);
+
+	UFUNCTION(BlueprintCallable, Category = "TimelineActorComp")
+		void PlayTimeline(float duration);
+
+	UFUNCTION(BlueprintCallable, Category = "TimelineActorComp")
+		void PlayTimelineFromPosition(float duration, float position);
+
+	UFUNCTION(BlueprintCallable, Category = "TimelineActorComp")
+		void ReverseTimelineFromEnd(float duration);
+
+	UFUNCTION(BlueprintCallable, Category = "TimelineActorComp")
+		void ReverseTimeline(float duration);
+
+	UFUNCTION(BlueprintCallable, Category = "TimelineActorComp")
+		void ReverseTimelineFromPosition(float duration, float position);
+
+	UFUNCTION(BlueprintCallable, Category = "TimelineActorComp")
+		void PlaySpecificPosition(float position);
+
+	UFUNCTION(BlueprintCallable, Category = "TimelineActorComp")
+		void StopTimeline();
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "TimelineActorComp")
+		float GetPlayBackPosition();
+
+	UFUNCTION(BlueprintCallable, Category = "TimelineActorComp")
+		void SetFloatCurve(UCurveFloat* NewCurveFloat);
+
+	UFUNCTION(BlueprintCallable, Category = "TimelineActorComp")
+		void SetTimelineLooping(bool NewLooping);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "TimelineActorComp")
+		bool GetTimelineLooping();
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "TimelineActorComp")
+		bool GetTimelinePlaying();
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "TimelineActorComp")
+		bool GetTimelineReversing();
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "TimelineActorComp")
+		float GetPlayRate();
+
+	UFUNCTION(BlueprintCallable, Category = "TimelineActorComp")
+		void SetPlayRate(float NewPlayRate);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "TimelineActorComp")
+		void OnTimelineStarted();
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "TimelineActorComp")
+		void OnTimelineFinished();
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "TimelinenActorComp")
+		void OutputCurveFloatValue(float Value);
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+	FTimeline Timeline;
+
+	UPROPERTY()
+		float playrate;
+
+	UPROPERTY()
+		float test;
+
+	UPROPERTY(EditAnywhere, Category = "TimelineActorComp")
+		UCurveFloat* CurveFloat;
+
+	UPROPERTY(BlueprintReadOnly, Category = "TimelineActorComp")
+		TEnumAsByte<ETimelineDirection::Type> TimelineDirection;
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
-		
+	
 };
